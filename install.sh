c58a2f12f8763cf9da318a98145c241f8699c136
rc <rc>
2020-08-10 08:28:45 -0700

    Added alias check

--- install.sh
+++ install.sh
@@ -3,6 +3,19 @@ if [ $? -ne 0 ]
 	then echo "Vim is not installed."
 	exit
 fi
+
+#Check if bashrc has vim alias
+grep -q "alias vi=\"vim\"" ~/.bashrc 
+if [ $? -ne 0 ] 
+	then echo "Can't find alias vi=\"vim\" in bashrc.  Add it (enter y to add)?"
+	read add
+	if [ $add=="y" ] 
+		then echo "alias vi=\"vim\"" >> ~/.bashrc
+	fi
+fi
+	
+
+
 cp cscope_maps.vim ~/
 cp vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
