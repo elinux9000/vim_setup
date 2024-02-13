c4d9e2ef59958fb4d08323e8c5a23e25e51d6dac
elinux9000 <35644969+elinux9000@users.noreply.github.com>
2024-02-13 11:17:13 -0600

    Update install.sh
    Removed extra spaces after EOF

--- install.sh
+++ install.sh
@@ -46,6 +46,6 @@ EOF
 cat <<EOF >> ~/.bashrc                                                                                 
 parse_git_branch() {                                                                                
         git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'                          
-}                                                                                                   
-EOF     
+}                                                                                          
+EOF
 
